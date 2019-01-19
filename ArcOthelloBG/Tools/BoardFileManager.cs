925120edca2b9a8143cf2a6b95fb19606b0c1fda
bull0n <lucas.bulloni@protonmail.ch>
2019-01-19 16:02:15 +0100

    forgot files

---
+++ ArcOthelloBG/Tools/BoardFileManager.cs
@@ -0,0 +1,33 @@
+﻿using ArcOthelloBG.Logic;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcOthelloBG
+{
+    class BoardFileManager
+    {
+        public static void SaveToFile(string filename, BoardState state)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(filename, FileMode.Create, FileAccess.Write, FileShare.None);
+            formatter.Serialize(stream, state);
+            stream.Close();
+        }
+
+        public static BoardState LoadStateFromFile(string filename)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read);
+            BoardState state = (BoardState)formatter.Deserialize(stream);
+            stream.Close();
+
+            return state;
+        }
+    }
+}
