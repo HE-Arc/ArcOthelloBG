ed14e01de967c865616c72150ad9b1953c929dfc
bull0n <lucas.bulloni@protonmail.ch>
2019-02-17 12:52:27 +0100

    forgot files

---
+++ ArcOthelloBG/IA/AlphaBetaAgentBG.cs
@@ -0,0 +1,67 @@
+﻿using ArcOthelloBG.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcOthelloBG.IA
+{
+    class AlphaBetaAgentBG
+    {
+        public Game IAGame;
+
+        public int whiteId;
+
+        public int blackId;
+
+        public AlphaBetaAgentBG(int columns, int rows, int whiteId, int blackId, int emptyId)
+        {
+            IAGame = Game.Instance;
+            IAGame.Init(columns, rows, whiteId, blackId, emptyId);
+            this.whiteId = whiteId;
+            this.blackId = blackId;
+        }
+
+        //Implemented from the code example in the pdf of the IA course
+        //the Tuple<int, int> contains in the following order: move X coordinate, move Y coordinate
+        public Tuple<int, Tuple<int, int>> alphabeta(Node root, int depth, int minOrMax, int parentValue)
+        {
+            if (depth == 0 || root.final())
+            {
+                Console.WriteLine(root.final());
+                Console.WriteLine(depth == 0);
+                return new Tuple<int, Tuple<int, int>>(root.eval(), null);
+            }
+
+            int optVal = minOrMax * -1 * int.MaxValue;
+
+            Tuple<int, int> optOp = null;
+
+            OthelloState initialState = Game.Instance.BoardState;
+
+            foreach (Tuple<int, int> op in root.ops())
+            {
+                Game.Instance.LoadState(initialState);
+                Node newNode = root.apply(op);
+                Tuple<int, Tuple<int, int>> result = alphabeta(newNode, depth - 1, -1 * minOrMax, optVal);
+                int val = result.Item1;
+
+                if (val * minOrMax > optVal * minOrMax)
+                {
+                    optVal = val;
+                    optOp = op;
+
+                    if (optVal * minOrMax > parentValue * minOrMax)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            Game.Instance.LoadState(initialState);
+
+            return new Tuple<int, Tuple<int, int>>(optVal, optOp);
+        }
+    }
+}
---
+++ ArcOthelloBG/IA/Node.cs
@@ -0,0 +1,76 @@
+﻿using ArcOthelloBG.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcOthelloBG.IA
+{
+    class Node
+    {
+        private AlphaBetaAgentBG agent;
+        private OthelloState state;
+        public int score;
+
+        public Node(AlphaBetaAgentBG agent)
+        {
+            this.agent = agent;
+            //We use the same Game instance for every node, we load the state and playerToPlay before playing/computing next moves
+            this.state = agent.IAGame.BoardState;
+            this.score = 0;
+        }
+
+        public bool final()
+        {
+            bool terminalNode = false;
+
+            // Game over?
+
+            return terminalNode;
+        }
+
+        public int eval()
+        {
+            //IAGame.PlayerToPlay = currentPlayerId;
+            //
+            //IAGame.Board = board;
+            //
+            //int totalScore = 0;
+            //
+            //if (currentPlayerId == IAGame.blackId)
+            //    totalScore += IAGame.BlackScore;
+            //else
+            //    totalScore += IAGame.WhiteScore;
+
+            return score;
+        }
+
+        public List<Tuple<int, int>> ops()
+        {
+            List<Tuple<int, int>> children = new List<Tuple<int, int>>();
+
+            this.state.AvailablePositions.ForEach(el => children.Add(el.toTuplesintint()));
+
+            return children;
+        }
+
+        public Node apply(Tuple<int, int> op)
+        {
+            Game.Instance.Play(new Vector2(op), this.state.PlayerId);
+
+            Node newNode = new Node(agent);
+
+            if (Game.Instance.PlayerToPlay == agent.blackId)
+            {
+                newNode.score = this.state.WhiteScore;
+            }
+            else
+            {
+                newNode.score = this.state.BlackScore;
+            }
+
+            return newNode;
+        }
+    }
+}
